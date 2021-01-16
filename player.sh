51a7f36166a2b38ac6619b0125cb2c4b04f46798
Nue-class Destroyer <nue@airmail.cc>
2021-01-16 12:22:41 +0100

    Added program files.

---
+++ player.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# File with the queue.
+QUEUE="queue"
+
+# Create queue file.
+[[ -f "$QUEUE" ]] && rm $QUEUE ; touch $QUEUE
+
+# Start queueing webserver.
+python web_server.py &
+PY_PID=$!
+
+# Cleanup finction.
+function finish {
+    kill $PY_PID
+    pkill mpv
+}
+trap finish EXIT
+
+# Main loop.
+while :; do
+    # Get first link in queue and play it if there is any.
+    VID=$(head -n 1 $QUEUE)
+    if [[ "$VID" != "" ]]; then
+        mpv $VID
+    
+        # Remove the link that was played.
+        tail -n +2 $QUEUE > "$QUEUE.tmp" && mv "$QUEUE.tmp" $QUEUE
+    else
+        # Wait a bit if there is nothing to play.
+        sleep 5
+    fi
+done
---
+++ web_server.py
@@ -0,0 +1,52 @@
+import cgi
+from wsgiref.simple_server import make_server
+
+html = '''
+<html>
+    <head>
+        <title>Add to tv queue</title>
+    </head>
+    <body>
+        <p>Add link to queue. Youtube and Twitch links work. Make sure the "http/https" part is present. You can also type Twitch channel names.</p>
+        <form method="post">
+            <label>Link</label>
+            <input type="text" name="item">
+            <input type="submit" value="Add">
+        </form>
+        <!--ADDED_TEXT-->
+    </body>
+</html>
+'''
+
+queue = 'queue'
+
+def app(environ, start_response):
+    response = html
+    if environ['REQUEST_METHOD'] == 'POST':
+        post_env = environ.copy()
+        post_env['QUERY_STRING'] = ''
+        post = cgi.FieldStorage(
+            fp=environ['wsgi.input'],
+            environ=post_env,
+            keep_blank_values=True
+        )
+
+        link = post['item'].value
+        # If link is no real website assume it is a Twitch channel.
+        if 'http' not in link:
+            link = 'https://twitch.tv/' + link
+            response = html.replace('<!--ADDED_TEXT-->', 'Twitch channel was added to queue.')
+        
+        else:
+            response = html.replace('<!--ADDED_TEXT-->', 'Link was added to queue.')
+
+        # Add link to queue.
+        with open(queue, 'a') as f:
+            f.write(link + '\n')
+
+    start_response('200 OK', [('Content-Type', 'text/html')])
+    return [bytes(response, 'utf-8')]
+
+print('Starting web server.')
+httpd = make_server('', 8000, app)
+httpd.serve_forever()
