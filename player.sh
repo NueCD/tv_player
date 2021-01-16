3303f78d920b307bcbc83e15d66f850a8397996f
NueCD <nue@airmail.cc>
2021-01-16 12:38:21 +0100

    Autochange working directory

--- player.sh
+++ player.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# Working directory.
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $DIR
+
+# Autoupdate
+git pull
+
 # File with the queue.
 QUEUE="queue"
 
